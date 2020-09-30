45585fc0dd6715ec75d264d3517b24b8755aec87
Pacome <pacome.prompsy@curie.fr>
2020-09-30 14:15:06 +0200

    CoOccurenceChIP_MM468

---
+++ Scripts/functions.R
@@ -0,0 +1,31 @@
+violin_plot_cooccurence <- function(
+    mat_init,mat_pers, sub_regions=rownames(mat_pers),
+    sub_cells = list(colnames(mat_pers),colnames(mat_init)),
+    levels=c("Untreated","Persister"),
+    colors = c("#dfdfdfff","#118675ff")){
+    
+    mat = Matrix::cBind(mat_pers,mat_init)
+    
+    # mat = mat[which(rownames(mat) %in% overexpressed_MM468_persister_regions),]
+    mat = mat[which(rownames(mat) %in% sub_regions),]
+    mat = mat[,which(colnames(mat) %in% unlist(sub_cells))]
+    bin_mat = mat
+    bin_mat[(bin_mat>0)]=1
+    
+    coocurrence_persister_genes_score = as.data.frame(Matrix::colSums(bin_mat) / nrow(bin_mat))
+    colnames(coocurrence_persister_genes_score) = "coocurrence_score"
+    rownames(coocurrence_persister_genes_score) = colnames(bin_mat)
+    coocurrence_persister_genes_score$sample = ""
+    coocurrence_persister_genes_score[sub_cells[[1]],"sample"] = levels[1]
+    coocurrence_persister_genes_score[sub_cells[[2]],"sample"] = levels[2]
+    
+    coocurrence_persister_genes_score$sample = factor(
+        coocurrence_persister_genes_score$sample,levels=levels)
+    
+    ggplot(coocurrence_persister_genes_score, aes(x=sample,y=coocurrence_score,
+                                                  fill=sample)) + 
+        geom_violin(alpha=0.8) + theme_classic() + scale_fill_manual(values=colors) +
+        stat_summary(fun=median, geom="point", size=2, color="black") + geom_jitter(size=0.2, alpha=0.2) + 
+        ggpubr::stat_compare_means(method = "t.test",ref.group = levels[1])
+    
+}
--- Scripts/scChIPseq/MM468/CooccurencescChIP.R
+++ Scripts/scChIPseq/MM468/CooccurencescChIP.R
@@ -1,15 +1,47 @@
 ######## GINI coefficients MM468 - scChIP ############
 
 library(here)
+library(Matrix)
+library(readxl)
+library(ggplot2)
+library(rtracklayer)
+library(GenomicRanges)
+library(geco.visu)
+library(geco.supervised)
+library(geco.unsupervised)
+library(ape)
+library(viridis)
+source(file.path(here(),"Scripts","functions.R"))
+source(file.path(here(),"Scripts","global_var.R"))
+
+# Parameters
+percent_coocurrence_to_define_top_cells = 0.4
 
 maindir = here()
+outdir = file.path(maindir,"output","scChIPseq","MM468","Coocurrence_K4")
+if(!dir.exists(outdir)) dir.create(outdir)
 load(file.path(maindir,"output","scRNAseq","common_over_genes_pers_vs_unt.RData"))
 
+# Find association Gene to Peak by using MM468 K4 peaks "consensus annotation" obtained from bulk ChIPseq 
+annot_MM468_K4 <- rtracklayer::import(file.path(maindir,"annotation","MM468_peaks_K4_29714_peaks.bed.gz"))
+load(file.path(maindir,"annotation","hg38.GeneTSS.rda"))
+hg38.GeneTSS = as(hg38.GeneTSS,"GRanges")
 annot10k <- read.table(unz(file.path(maindir,"annotation","gencode.v34.annotation.transcriptTSS_10k.bed.zip"),
                            "gencode.v34.annotation.transcriptTSS_10k.bed"))
-
 colnames(annot10k) <- c("chr","start","end","transcripts","gene","sense")
-annot10k$ID <- paste(paste(annot10k$chr,annot10k$start,sep=":"),annot10k$end,sep="-")
+annot10k = as(annot10k[,c(1:3,5)],"GRanges")
+annot10k$ID = paste0(seqnames(annot10k),":",start(annot10k),"-",end(annot10k))
+  
+differential_results_scRNA <- readxl::read_xlsx(file.path(
+  maindir,"output","scRNAseq","MM468","Persister","Supervised","Tables",
+  "Differential_analysis_Limma.xlsx"),
+  sheet=1)
+
+overexpressed_MM468_persister_genes = differential_results_scRNA$Symbol
+transcripts = annot10k[which(annot10k$gene %in% overexpressed_MM468_persister_genes)]
+regions = hg38.GeneTSS[which(hg38.GeneTSS$gene %in% overexpressed_MM468_persister_genes)]
+regions = transcripts[subjectHits(findOverlaps(regions,transcripts)),]
+overexpressed_MM468_persister_regions = regions$ID
 
 # house_keeping_genes = c("GAPDH","VGF","CCNA2","LMNA","GAPDH","UBB","UBC")
 # From  https://www.genomics-online.com/resources/16/5049/housekeeping-genes/
@@ -19,53 +51,53 @@ house_keeping_genes = c("GAPDH","ACTB","RRN18S","PGK1","PPIA","RPL13A","RPLP0",
                         "ARBP","B2M","YWHAZ","SDHA","TFRC","GUSB","HMBS","HPRT1","TBP")
 house_keeping_genes_row <- c(11745,52102,60829,52851,30219,14438,56781,45738,42778,53080,11123,51916) #manually annotated with the right TSS
 annot10k_housekeeping <- annot10k[house_keeping_genes_row,]
-
-K27me3_control_regions <-
-
-#diff_genes <-  c("NNMT","TAGLN","INHBA","KRT6A","KRT6B","KRT16","KRT14","KLK10","KLK5","KLK7")
-#diff_genes_figures <-  c("BMP6","CDKN2B","TGFBR2","TGFBR3","TGFB2","INHBA","INHBB","SMAD2","TGFB1","FOSL1","NNMT","KRT14","KLK10","KLK5","TAGLN","NNMT","ELN","TGFBR3")
+house_keeping_genes_region = annot10k_housekeeping$ID
 
 bivalent_genes <- c("TGFB1","BMP6","KLK10","ABCC4","FOSL1","VIM","COL4A2","LAMB1","KRT14")
 bivalent_genes_row <- c(29719,49126,30317,15839,9916,6113,16055,53950,24851)
 annot10k_bivalent <- annot10k[bivalent_genes_row,]
 
-
+file_names = unzip(file.path(
+  maindir,"input","scChIPseq","MM468","MM468_K4_transcripts_10k_1000.zip"),
+  list = T)$Name
+list_files = vector("list", length(file_names))
+names(list_files) = gsub(".tsv","",file_names)
+path_matrices = file.path(  maindir,"input","scChIPseq",
+                            "MM468","MM468_K4_transcripts_10k_1000.zip")
 
 #10k counts
-
-K4_5FU6_day60 <- read.table("MM468_K4_transcripts_10k_1000/MM468_5FU6_D60_K4.tsv")
-K4_DMSOi <- read.table("MM468_K4_transcripts_10k_1000/MM468_Ipop_DMSO6_K4me3.tsv")
-K4_DMSO1_day131 <- read.table("MM468_K4_transcripts_10k_1000/MM468_Rpop_DMSO1_K4me3.tsv")
-K27_5FU6_day33 <- read.table("MM468_K27_transcripts_10k_1000/MM468_5FU6_day33_K27.tsv")
-K27_DMSO6_day60 <- read.table("MM468_K27_transcripts_10k_1000/MM468_DMSO6_day60_K27.tsv")
-
+for(file in names(list_files)) {
+  list_files[[file]] = scater::readSparseCounts(
+    unzip(zipfile = path_matrices,files = paste0(file,".tsv")))
+  colnames(list_files[[file]]) = paste0(file,"_",colnames(list_files[[file]]))
+}
 
 #Determine the cells with the top cells with the most efficient IP (the most presence of H3K4me3 fragments for housekeeping genes)
-mat <- K4_DMSOi
+mat <- list_files$MM468_Ipop_DMSO6_K4me3
 mat_housekeeping <- mat[house_keeping_genes_row,]
 bin_mat = mat_housekeeping
 bin_mat[(bin_mat>0)]=1
-coocurrence_K4me3_score_housekeeping = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
-top_cells <- rownames(coocurrence_K4me3_score_housekeeping)[which(coocurrence_K4me3_score_housekeeping$`colSums(bin_mat)/nrow(bin_mat)`>0.4)]
+coocurrence_K4me3_score_housekeeping = as.data.frame(Matrix::colSums(bin_mat) / nrow(bin_mat))
+colnames(coocurrence_K4me3_score_housekeeping) = "Coocurrence_score"
+top_cells_init <- rownames(coocurrence_K4me3_score_housekeeping)[which(
+  coocurrence_K4me3_score_housekeeping$Coocurrence_score>percent_coocurrence_to_define_top_cells)]
 
-mat_top  <- mat[,top_cells]
-#bin_mat[bin_mat>0] = 1
+mat_top  <- mat[,top_cells_init]
 Gin_unt = edgeR::gini(t(mat_top))
 Gini_focus = Gin_unt[which(names(Gin_unt) %in% c(annot10k_housekeeping$ID,annot10k_bivalent$ID))]
 Gini_focus = data.frame(Gini_focus,0)
 colorlist = ifelse(rownames(Gini_focus) %in% annot10k_housekeeping$ID, "grey", "red")
 Gini_focus_DMSOi <- Gini_focus
 
-
 mat_bivalent <- mat_top[bivalent_genes_row,]
 mat_bivalent_DMSOi <- mat_bivalent
 
 bin_mat = mat_bivalent
 bin_mat[(bin_mat>0)]=1
-coocurrence_K4me3_score_bivalent_DMSOi = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
+coocurrence_K4me3_score_bivalent_DMSOi = as.data.frame(Matrix::colSums(bin_mat) / nrow(bin_mat))
+colnames(coocurrence_K4me3_score_bivalent_DMSOi) = "Coocurrence_score"
 
-
-pdf(file.path("GiniScores_H3K4me3_bivalent_genes_inK4_DMSOi.pdf"),width = 12)
+pdf(file.path(outdir,"GiniScores_H3K4me3_bivalent_genes_inK4_DMSOi.pdf"),width = 12)
 plot(0,0,type="n",xlim=c(-0.06,1.06), ylim=c(0,2), yaxt = 'n', main ="Gini Scores")
 for (i in 1:length(Gini_focus$Gini_focus)) { stripchart(Gini_focus$Gini_focus[i],
                                                         add = T, bg = colorlist[i],
@@ -77,14 +109,17 @@ text(Gini_focus$Gini_focus,
 dev.off()
 
 #Determine the cells with the top cells with the most efficient IP (the most presence of H3K4me3 fragments for housekeeping genes)
-mat <- K4_5FU6_day60
+mat <- list_files$MM468_5FU6_D60_K4
 mat_housekeeping <- mat[house_keeping_genes_row,]
 bin_mat = mat_housekeeping
 bin_mat[(bin_mat>0)]=1
 coocurrence_K4me3_score_housekeeping = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
-top_cells <- rownames(coocurrence_K4me3_score_housekeeping)[which(coocurrence_K4me3_score_housekeeping$`colSums(bin_mat)/nrow(bin_mat)`>0.4)]
+colnames(coocurrence_K4me3_score_housekeeping) = "Coocurrence_score"
 
-mat_top  <- mat[,top_cells]
+top_cells_pers <- rownames(coocurrence_K4me3_score_housekeeping)[which(
+  coocurrence_K4me3_score_housekeeping$Coocurrence_score>percent_coocurrence_to_define_top_cells)]
+
+mat_top  <- mat[,top_cells_pers]
 #bin_mat[bin_mat>0] = 1
 Gin_unt = edgeR::gini(t(mat_top))
 Gini_focus = Gin_unt[which(names(Gin_unt) %in% c(annot10k_housekeeping$ID,annot10k_bivalent$ID))]
@@ -97,9 +132,9 @@ mat_bivalent_5FU6 <- mat_bivalent
 bin_mat = mat_bivalent
 bin_mat[(bin_mat>0)]=1
 coocurrence_K4me3_score_bivalent_5FU6 = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
+colnames(coocurrence_K4me3_score_bivalent_5FU6) = "Coocurrence_score"
 
-
-pdf(file.path("GiniScores_H3K4me3_bivalent_genes_in5FU6.pdf"),width = 12)
+pdf(file.path(outdir,"GiniScores_H3K4me3_bivalent_genes_in5FU6.pdf"),width = 12)
 plot(0,0,type="n",xlim=c(-0.06,1.06), ylim=c(0,2), yaxt = 'n', main ="Gini Scores")
 for (i in 1:length(Gini_focus$Gini_focus)) { stripchart(Gini_focus$Gini_focus[i],
                                                         add = T, bg = colorlist[i],
@@ -111,41 +146,79 @@ text(Gini_focus$Gini_focus,
 dev.off()
 
 
+pdf(file.path(outdir,"GiniScores_H3K4me3_5FU6_vs_DMSO_dotplot.pdf"),width = 12)
 plot(Gini_focus_5FU6$Gini_focus~Gini_focus_DMSOi$Gini_focus,col=colorlist,pch=19,ylim=c(0.3,1),xlim=c(0.3,1))
 abline(a=0,b=1)
-text(Gini_focus_5FU6$Gini_focus~Gini_focus_DMSOi$Gini_focus,
-     
-     cex = 0.75,
+text(Gini_focus_5FU6$Gini_focus~Gini_focus_DMSOi$Gini_focus,cex = 0.75,
      labels = annot10k$gene[annot10k$ID %in% rownames(Gini_focus)], srt = 75, col = colorlist)
+dev.off()
+
+
+# Violin plot co-occurence score
+mat_pers = list_files$MM468_5FU6_D60_K4
+mat_init = list_files$MM468_Ipop_DMSO6_K4me3
+
+png(file.path(outdir,paste0("Coocurrence_score_overpers_K4_violin.png")),height=1000,width=1500,res=300)
+violin_plot_cooccurence(mat_init,mat_pers,
+                        sub_regions =  overexpressed_MM468_persister_regions,
+                        sub_cells = list(top_cells_init,top_cells_pers))
+dev.off()
+
+png(file.path(outdir,paste0("Coocurrence_score_hk_K4_violin.png")),height=1000,width=1500,res=300)
+violin_plot_cooccurence(mat_init,mat_pers,
+                        sub_regions =  house_keeping_genes_region,
+                        sub_cells = list(top_cells_init,top_cells_pers))
+dev.off()
 
 #Heatmap on top cells of DMSOi and 5FU6
-tmat <- cbind(mat_bivalent_5FU6,mat_bivalent_DMSOi)
-distHC <- c("distPearson","distCosine","euclidean","maximum","manhattan","canberra","binary","minkowski")[3]
-methHC <- c("ward","ward.D","ward.D2","single","complete","average")[3]
-if(distHC=="distPearson")	d <- distPearson(tmat)
-if(distHC=="distCosine")	d <- distCosine(tmat)
-if(distHC %in% c("euclidean","maximum","manhattan","canberra","binary","minkowski"))	
-  d <- dist(tmat,distHC)
-hc <- hclust(d,method=methHC)
+tmat = Matrix::cBind(mat_pers,mat_init)
+# tmat = tmat[-which(rowSums(tmat)==0),]
+tmat = tmat[match(c(overexpressed_MM468_persister_regions,house_keeping_genes_region),rownames(tmat)),
+            match(c(top_cells_pers,top_cells_init),colnames(tmat))]
+bin_mat = tmat
+bin_mat[bin_mat>0] = 1
 
-mat.so <- as.matrix(cor_genes[hc_genes$order,hc_genes$order])
-if(chRangeHM){
-  for(i in 1:nrow(mat.so)){
-    mat.so[i,] <- geco.changeRange(mat.so[i,],newmin=0,newmax=1)
-  }
-}
-png(file.path(resdir,paste0("Cooccurrence_pers_genes_heatmap_",name,".png")), height=8000,width=8000,res=600)
-geco.hclustAnnotHeatmapPlot(x=(mat.so),
-                            hc=hc,
-                            hmColors=hmColors,
-                            anocol=anocol_genes[hc_genes$order,],#[,ncol(cc.col):1]
-                            xpos=c(0.15,0.9,0.164,0.885),
-                            ypos=c(0.1,0.5,0.5,0.6,0.62,0.95),
-                            dendro.cex=0.01,
-                            xlab.cex=0.3,
-                            hmRowNames=TRUE,
-                            hmRowNames.cex=0.1
-)
+methHC <- c("ward","ward.D","ward.D2","single","complete","average")[3]
+hc <- hclust(as.dist(1-cor(as.matrix(bin_mat))),method=methHC)
+# chRangeHM = TRUE
+mat.so <- as.matrix(bin_mat)
+# if(chRangeHM){
+#   for(i in 1:nrow(mat.so)){
+#     mat.so[i,] <- geco.changeRange(mat.so[i,],newmin=0,newmax=1)
+#   }
+# }
+
+#Create cell annotation data.frame
+annot_cell = data.frame(cell_id = c(top_cells_pers,top_cells_init),
+                        sample_id = c(rep("Persister",length(top_cells_pers)),
+                                      rep("Initital",length(top_cells_init))))
+rownames(annot_cell) = annot_cell$cell_id
+anocol = geco.annotToCol4(annot_cell)
+
+hc_gene = hclust(dist(as.matrix(bin_mat)),method=methHC)
+hc_gene$labels = rep("",length(hc_gene$labels))
+#Create gene/region annotation data.frame
+annot_gene = data.frame(region = c(overexpressed_MM468_persister_regions,house_keeping_genes_region),
+                        genes = c(as.character(regions$gene),as.character(annot10k_housekeeping$gene)),
+                        type = c(rep("Persister",length(overexpressed_MM468_persister_regions)),
+                                   rep("Housekeeping",length(house_keeping_genes_region))),
+                        total_cells = rowSums(bin_mat))
+rownames(annot_gene) = annot_gene$region
+anocol_gene = geco.annotToCol4(annot_gene)
+
+png(file.path(outdir,paste0("Cooccurrence_pers_hk_topcells_K4_heatmap.png")),
+    height=8000,width=8000,res=600)
+geco.hclustAnnotHeatmapPlot.withColumn(
+  x=mat.so[hc_gene$order,hc$order],
+  hc=hc,
+  hmColors=hmColors,
+  anocol=anocol[hc$order,],
+  hc_row = hc_gene,
+  anorow = anocol_gene[hc_gene$order,],
+  dendro.cex=0.01,
+  xlab.cex=1,
+  hmRowNames=FALSE,
+  hmRowNames.cex=0.5)
 dev.off()
 
 
@@ -154,36 +227,39 @@ dev.off()
 ###############################################################
 
 #Use peak annot to select top cells and then try to switch to 10k
-annotZerone
-M5FU6_day33 <- read.table("MM468_K27_peaks_1000/MM468_5FU6_day33.tsv")
-DMSO6_day60 <- read.table("MM468_K27_peaks_1000/MM468_DMSO6_day60.tsv")
-
-
-loci_control <- c("chr7_127610001_127640000","chr7_127610001_127640000","chr19_34686001_34730000","chr19_13276001_13725000","chr19_15609001_16005000","chr19_50514001_50788000","chr20_16764001_17491000")
-gene_control <- ("CACNA1A","PAX4","ZNF302","CYP4F2","SHANK1","PCSK2")
-
-mat <- DMSO6_day60
-mat_loci_control <- mat[loci_control,]
-bin_mat = mat_loci_control
-bin_mat[(bin_mat<2)]=0
-bin_mat[(bin_mat>1)]=1
-
-coocurrence_K4me3_score_housekeeping = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
-top_cells <- rownames(coocurrence_K4me3_score_housekeeping)[which(coocurrence_K4me3_score_housekeeping$`colSums(bin_mat)/nrow(bin_mat)`>0.4)]
-
-
-annotZerone$Gini_K4_DMSO6_day60 <- Gin_unt
-Gini_focus = Gin_unt[which(names(Gin_unt) %in% c(house_keeping_genes,overexpressed_pers_genes))]
-Gini_focus = data.frame(Gini_focus,0)
-
-colorlist = ifelse(rownames(Gini_focus) %in% house_keeping_genes, "grey", "red")
-pdf(file.path(resdir,"GiniScores_common_pers_genes_hk_inInitial.pdf"),width = 12)
-plot(0,0,type="n",xlim=c(-0.06,1.06), ylim=c(0,2), yaxt = 'n', main ="Gini Scores")
-for (i in 1:length(Gini_focus$Gini_focus)) { stripchart(Gini_focus$Gini_focus[i],
-                                                        add = T, bg = colorlist[i],
-                                                        vertical = F, pch =21, method = "jitter") }
-text(Gini_focus$Gini_focus,
-     (runif(length(Gini_focus$Gini_focus))/2) + rep(c(0,1.5),length(Gini_focus$Gini_focus)/2), 
-     cex = 0.75,
-     labels = rownames(Gini_focus), srt = 75, col = colorlist)
-dev.off()
+# samples = c("MM468_5FU6_day33","MM468_DMSO6_day60")
+# for(file in samples){
+#   
+# }
+# M5FU6_day33 <- read.table("MM468_K27_peaks_1000/MM468_5FU6_day33.tsv")
+# DMSO6_day60 <- read.table("MM468_K27_peaks_1000/MM468_DMSO6_day60.tsv")
+# 
+# 
+# loci_control <- c("chr7_127610001_127640000","chr7_127610001_127640000","chr19_34686001_34730000","chr19_13276001_13725000","chr19_15609001_16005000","chr19_50514001_50788000","chr20_16764001_17491000")
+# gene_control <- c("CACNA1A","PAX4","ZNF302","CYP4F2","SHANK1","PCSK2")
+# 
+# mat <- DMSO6_day60
+# mat_loci_control <- mat[loci_control,]
+# bin_mat = mat_loci_control
+# bin_mat[(bin_mat<2)]=0
+# bin_mat[(bin_mat>1)]=1
+# 
+# coocurrence_K4me3_score_housekeeping = as.data.frame(colSums(bin_mat) / nrow(bin_mat))
+# top_cells <- rownames(coocurrence_K4me3_score_housekeeping)[which(coocurrence_K4me3_score_housekeeping$`colSums(bin_mat)/nrow(bin_mat)`>percent_coocurrence_to_define_top_cells)]
+# 
+# 
+# annotZerone$Gini_K4_DMSO6_day60 <- Gin_unt
+# Gini_focus = Gin_unt[which(names(Gin_unt) %in% c(house_keeping_genes,overexpressed_pers_genes))]
+# Gini_focus = data.frame(Gini_focus,0)
+# 
+# colorlist = ifelse(rownames(Gini_focus) %in% house_keeping_genes, "grey", "red")
+# pdf(file.path(outdir,"GiniScores_common_pers_genes_hk_inInitial.pdf"),width = 12)
+# plot(0,0,type="n",xlim=c(-0.06,1.06), ylim=c(0,2), yaxt = 'n', main ="Gini Scores")
+# for (i in 1:length(Gini_focus$Gini_focus)) { stripchart(Gini_focus$Gini_focus[i],
+#                                                         add = T, bg = colorlist[i],
+#                                                         vertical = F, pch =21, method = "jitter") }
+# text(Gini_focus$Gini_focus,
+#      (runif(length(Gini_focus$Gini_focus))/2) + rep(c(0,1.5),length(Gini_focus$Gini_focus)/2), 
+#      cex = 0.75,
+#      labels = rownames(Gini_focus), srt = 75, col = colorlist)
+# dev.off()
